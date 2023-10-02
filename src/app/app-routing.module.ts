49837051d6a6bd64231d9910420ff0f5ce5bb2db
Luis <luis.monedero8@gmail.com>
2023-10-02 14:14:51 +0200

    fix(*): solved problems with eslint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { GameComponent } from './pages/game/game.component';
+import { NgModule } from '@angular/core'
+import { RouterModule, type Routes } from '@angular/router'
+import { HomeComponent } from './pages/home/home.component'
+import { GameComponent } from './pages/game/game.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'home/:id', component: GameComponent }
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
